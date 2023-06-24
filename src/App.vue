57a394fa8f3f571f24db35f65f0709e5510ef649
southliu <1275093225@qq.com>
2023-06-24 12:05:12 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <input />
+</template>
+
+<style scoped>
+
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    open: true
+  },
+  // 去除console和debugger
+  esbuild: {
+    pure: ["console.log", "debugger"]
+  },
+})
